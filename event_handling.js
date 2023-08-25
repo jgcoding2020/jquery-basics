51805982b756f57535ea4faa31e1d74e9ff39008
jgcoding2020 <jgardn11@yahoo.com>
2023-08-25 10:51:28 -0400

    Event handling

--- event_handling.js
+++ event_handling.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Wait for the document to be fully loaded
 
     // Click event handler
-    $("#clickMe").click(function() {
+    $("#clickMe").click(function () {
         alert("Button clicked!");
     });
 
     // Hover event handler
     $("#hoverMe").hover(
-        function() {
+        function () {
             $(this).text("Mouse over!");
         },
-        function() {
+        function () {
             $(this).text("Hover Me");
         }
     );
 
     // Keypress event handler
-    $("#keypressMe").keypress(function(event) {
+    $("#keypressMe").keypress(function (event) {
         var keyPressed = event.key;
         alert("Key pressed: " + keyPressed);
     });
 
     // Click event handler with text change
-    $("#changeMe").click(function() {
+    $("#changeMe").click(function () {
         $(this).text("Text changed by click event!");
     });
 });
