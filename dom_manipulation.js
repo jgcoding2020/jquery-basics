fe40e310b8b11408371d226864428fffe4b837dc
jgcoding2020 <jgardn11@yahoo.com>
2023-08-25 10:28:47 -0400

    DOM manipulation

---
+++ dom_manipulation.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    // Wait for the document to be fully loaded
+
+    // Select the element with the ID 'myElement'
+    var $myElement = $("#myElement");
+
+    // Select all list items within the 'myList' element
+    var $listItems = $("#myList li");
+
+    // Change the text content and background color of 'myElement' when the button is clicked
+    $("#changeContent").click(function() {
+        $myElement.text("Text changed by jQuery!").css("background-color", "lightblue");
+    });
+
+    // Add a new list item when the button is clicked
+    $("#addListItem").click(function() {
+        var newItem = $("<li>New item</li>");
+        $listItems.last().after(newItem);
+    });
+
+    // Remove the last list item when the button is clicked
+    $("#removeListItem").click(function() {
+        $listItems.last().remove();
+    });
+
+    // Add a click event handler to each list item to change its text when clicked
+    $listItems.click(function() {
+        var $this = $(this);
+        $this.text("Clicked: " + $this.text());
+    });
+});
